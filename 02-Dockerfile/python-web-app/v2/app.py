7af1cfa29e3180c892630ba9446a8a96de1a8bf3
root <root@master.example.com>
2021-08-17 09:27:40 +0000

    Python-web-app

---
+++ 02-Dockerfile/python-web-app/v2/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import os 
+import socket
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "Hello World"
+
+@app.route("/hello")
+def hello_test():
+    html="<h2> Hello from docker based python web server.!!!</h2>"
+    return html
+
+
+@app.route("/info")
+def hello_info(): 
+    html="<h2> Hey Python Web Server</h2>"\
+         "<b> Hostname: </b> {hostname}<br/>" \
+         "<b> IP Address: </b> {fqdn}<br/>"
+    return html.format(hostname=socket.gethostname(),fqdn=socket.gethostbyname(socket.gethostname()))
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8081)
