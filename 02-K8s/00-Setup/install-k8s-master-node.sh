6c80e5c6bdb5e008f6f809e5c8b1d089be7376f8
root <root@terraform.example.com>
2021-08-18 05:56:53 +0000

    02-K8s

--- 02-K8s/00-Setup/install-k8s-master-node.sh
+++ 02-K8s/00-Setup/install-k8s-master-node.sh
@@ -28,7 +28,7 @@ echo "******************* Deploying kubernetes ***********************"
 
 # Fix Me - Update the "apiserver-advertise-address" with your master node ip.
 
-kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="172.31.3.195" 
+kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="172.31.0.100" 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 
